041d28f47c0849d6387fca4aaa05e7e78c5560e4
Uliana3107 <38219966+Uliana3107@users.noreply.github.com>
2018-06-01 17:22:54 +0300

    Add files via upload

--- Unit3.pas
+++ Unit3.pas
@@ -32,8 +32,9 @@ type
     procedure Execute; override;
   end;
 
-var t,p,p1: double;
+var t,p1,p2,p3: double;
 M1,M2,M3,D1,D2,D3,Z1,Z2,Z3,C1,C2,C3:double;
+kol1,kol2,kol3:integer;
 //-----------//
 implementation
 uses unit1;
@@ -61,32 +62,37 @@ end;
 
 
 procedure Tnew.DoWork;
-var i: integer;
+var q1:Integer;
 begin
-q:=1;
-  MMX(M1,q,w1);
-  DMX(D1,C1,Z1,q,w1);
-
+q1:=1;
+  p1:=1/w1;
+  if q1=w1 then kol1:=w1 else kol1:=w1-q1;
+  MMX(M1,p1,q1,w1);
+  DMX(D1,C1,Z1,q1,w1);
   Terminate;
 end;
 //----//
  procedure Tnew2.DoWork;
- var i: integer;
+ var i,q2: integer;
  begin
-  q:= w1+1;
-   MMX(M2,q,w2);
-   DMX(D2,C2,Z2,q,w2);
+  //q:= w1;
+    p2:=1/(w2-w1);
+   q2:=w1+1;
+   if q2=w2 then kol2:=w2 else kol2:=w2-w1;
+   MMX(M2,p2,q2,w2);
+   DMX(D2,C2,Z2,q2,w2);
   terminate;
  end;
 //------//
 procedure Tnew3.DoWork;
-var i: integer; t:double;
+var i,q3: integer; 
 begin
- q:= w2+1;
-  MMX(M3,q,w3);
-  DMX(D3,C3,Z3,q,w3);
-
-
+ //q:= w2;
+  p3:=1/(w3-w2);
+  q3:=w2+1;
+  if q3=w3 then kol3:=w3 else kol3:=w3-w2;
+  MMX(M3,p3,q3,w3);
+  DMX(D3,C3,Z3,q3,w3);
+  terminate;
 end;
-
 end.
